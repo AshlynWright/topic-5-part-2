bdab39e4adeb18bad230b1c09b6e2b9de3609655
Ashlyn Wright <362581399@GoTVDSB.ca>
2025-10-27 09:05:18 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace topic_5_part_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Ashlyn wright
+            //Topic 5 part 2
+            //part 2 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
